0d6d46864358ee7b788fb1ed385f0299bd64b8c7
Melvin Thu <81542789+melthu@users.noreply.github.com>
2026-06-11 14:59:17 -0400

    Add: jump-game-ii - submission-8

---
+++ Data Structures & Algorithms/jump-game-ii/submission-8.py
@@ -0,0 +1,20 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        
+
+        e = 0
+        ne = 0
+        c = 0
+
+        for i in range(n-1):
+            ne = max(ne, i + nums[i])
+
+            if i == e:
+                e = ne
+                c += 1
+                if e >= n-1:
+                    return c
+
+        return c
