2eecc07b40f50a23ae2b39e184fdd07d35f576e9
Melvin Thu <81542789+melthu@users.noreply.github.com>
2026-06-21 22:54:48 -0400

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        a = [0] * 26
+        b = [0] * 26
+        for i in range(len(s)):
+            a[ord(s[i]) % 26] += 1
+            b[ord(t[i]) % 26] += 1
+        
+        return a == b
+
+        
+        
